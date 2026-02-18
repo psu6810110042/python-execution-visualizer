19c6d242a2779957f2f48d7db460a48ac0c76b38
Jirakorn Sukmee <6810110042@psu.ac.th>
2026-02-18 23:12:42 +0700

    Added: kivy dependencies and sample kivy program

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from kivy-app-project!")
+import kivy
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+    def build(self):
+        return Label(text="Hello, world!")
 
 
 if __name__ == "__main__":
-    main()
+    MyApp().run()

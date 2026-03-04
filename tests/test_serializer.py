017742f5d8a72ebaa24c670e0d9be470f21be18d
Jirakorn Sukmee <6810110042@psu.ac.th>
2026-03-04 13:51:09 +0700

    fixed: test_serializer expecting wrong object

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -27,107 +27,120 @@ class TestSerializer(unittest.TestCase):
         self.assertEqual(self.serializer.serialize("hello world"), "hello world")
 
     def test_lists_and_tuples(self):
-        self.assertEqual(self.serializer.serialize([1, 2, 3]), [1, 2, 3])
-        self.assertEqual(self.serializer.serialize((1, 2, 3)), [1, 2, 3])
+        res_list = self.serializer.serialize([1, 2, 3])
+        self.assertEqual(res_list["__type__"], "list")
+        self.assertEqual(res_list["value"], [1, 2, 3])
+
+        res_tuple = self.serializer.serialize((1, 2, 3))
+        self.assertEqual(res_tuple["__type__"], "tuple")
+        self.assertEqual(res_tuple["value"], [1, 2, 3])
 
     def test_list_truncation(self):
         long_list = [1, 2, 3, 4, 5, 6, 7]
         result = self.serializer.serialize(long_list)
-        self.assertEqual(len(result), 6)
-        self.assertEqual(result[:5], [1, 2, 3, 4, 5])
-        self.assertEqual(result[5], "<truncated, total 7>")
+        val = result["value"]
+        self.assertEqual(len(val), 6)
+        self.assertEqual(val[:5], [1, 2, 3, 4, 5])
+        self.assertEqual(val[5], "<truncated>")
 
     def test_sets(self):
         s = {1, 2, 3}
         result = self.serializer.serialize(s)
         self.assertIsInstance(result, dict)
-        self.assertEqual(result["__type"], "set")
-        self.assertEqual(sorted(result["items"]), [1, 2, 3])
+        self.assertEqual(result["__type__"], "set")
+        self.assertEqual(sorted(result["value"]), [1, 2, 3])
 
     def test_set_truncation(self):
         s = {1, 2, 3, 4, 5, 6}
         result = self.serializer.serialize(s)
-        self.assertEqual(result["__type"], "set")
-        self.assertEqual(len(result["items"]), 6)
-        self.assertEqual(result["items"][-1], "<truncated, total 6>")
+        self.assertEqual(result["__type__"], "set")
+        self.assertEqual(len(result["value"]), 6)
+        self.assertEqual(result["value"][-1], "<truncated>")
 
     def test_dicts(self):
         d = {"a": 1, "b": 2}
-        self.assertEqual(self.serializer.serialize(d), {"a": 1, "b": 2})
+        res = self.serializer.serialize(d)
+        self.assertEqual(res["__type__"], "dict")
+        self.assertEqual(res["value"], {"a": 1, "b": 2})
 
     def test_dict_truncation(self):
         d = {str(i): i for i in range(10)}
         result = self.serializer.serialize(d)
-        self.assertEqual(len(result), 6)
-        self.assertEqual(result["__truncated"], "total 10")
+        self.assertEqual(result["__type__"], "dict")
+        self.assertEqual(len(result["value"]), 6)
+        self.assertEqual(result["value"]["__truncated"], "total 10")
 
     def test_circular_reference_list(self):
         l = [1, 2]
         l.append(l)
         result = self.serializer.serialize(l)
-        self.assertEqual(result[:2], [1, 2])
-        self.assertTrue(result[2].startswith("<Circular Reference "))
+        val = result["value"]
+        self.assertEqual(val[:2], [1, 2])
+        self.assertTrue(val[2].startswith("<Circular Reference "))
 
     def test_circular_reference_dict(self):
         d = {"a": 1}
         d["self"] = d
         result = self.serializer.serialize(d)
-        self.assertEqual(result["a"], 1)
-        self.assertTrue(result["self"].startswith("<Circular Reference "))
+        val = result["value"]
+        self.assertEqual(val["a"], 1)
+        self.assertTrue(val["self"].startswith("<Circular Reference "))
 
-    # Nested 5 levels deep
-    # max_depth is 3
-    # Level 0: [
-    # Level 1:   [
-    # Level 2:     [
-    # Level 3:       [
-    # Level 4:         (cutoff -> lists hit type(...) string)
     def test_max_depth_recursion(self):
         nested = [[[[[1]]]]]
         result = self.serializer.serialize(nested)
-        self.assertEqual(result, [[[["list(...)"]]]])
+        self.assertEqual(result["value"][0]["value"][0]["value"][0]["value"], ["list(...)"])
 
     def test_functions_and_modules(self):
         def my_test_func():
             pass
 
         func_res = self.serializer.serialize(my_test_func)
-        self.assertEqual(func_res, "<function my_test_func>")
+        self.assertEqual(func_res["__type__"], "function")
+        self.assertEqual(func_res["value"], "my_test_func")
 
         mod_res = self.serializer.serialize(sys)
-        self.assertEqual(mod_res, "<module sys>")
+        self.assertEqual(mod_res["__type__"], "module")
+        self.assertEqual(mod_res["value"], "sys")
 
     def test_generators(self):
         def string_yielder():
             yield "a"
 
         gen_res = self.serializer.serialize(string_yielder())
-        self.assertEqual(gen_res, "<generator string_yielder>")
+        self.assertEqual(gen_res["__type__"], "generator")
+        self.assertEqual(gen_res["value"], "string_yielder")
 
     def test_bytes_and_bytearray(self):
         b = b"hello"
-        self.assertEqual(self.serializer.serialize(b), "b'68656c6c6f'")
+        res_b = self.serializer.serialize(b)
+        self.assertEqual(res_b["__type__"], "bytes")
+        self.assertEqual(res_b["value"], "b'68656c6c6f'")
 
         # Test large bytes truncation
         long_b = b"x" * 60
-        res = self.serializer.serialize(long_b)
-        self.assertTrue(res.endswith("...' (60 bytes)"))
+        res_long = self.serializer.serialize(long_b)
+        self.assertTrue(res_long["value"].endswith("...' (60 bytes)"))
 
         ba = bytearray([1, 2, 3])
-        self.assertEqual(self.serializer.serialize(ba), "bytearray([1, 2, 3])")
+        res_ba = self.serializer.serialize(ba)
+        self.assertEqual(res_ba["__type__"], "bytearray")
+        self.assertEqual(res_ba["value"], "bytearray([1, 2, 3])")
 
     def test_range(self):
         r = range(2, 20, 3)
-        self.assertEqual(self.serializer.serialize(r), "range(2, 20, 3)")
+        res = self.serializer.serialize(r)
+        self.assertEqual(res["__type__"], "range")
+        self.assertEqual(res["value"], "range(2, 20, 3)")
 
     def test_custom_objects(self):
         obj = DummyClass(100)
         result = self.serializer.serialize(obj)
         self.assertIsInstance(result, dict)
-        self.assertEqual(result["__type"], "DummyClass")
-        self.assertIn("__id", result)
-        self.assertIn("attributes", result)
-        self.assertEqual(result["attributes"]["val"], 100)
+        self.assertEqual(result["__type__"], "DummyClass")
+        self.assertIn("__ref__", result)
+        self.assertIn("value", result)
+        self.assertEqual(result["value"]["value"]["val"], 100)
 
     def test_circular_custom_objects(self):
         local_serializer = Serializer(max_depth=5)
@@ -138,35 +151,36 @@ class TestSerializer(unittest.TestCase):
         obj2.other = obj1
 
         result = local_serializer.serialize(obj1)
-        self.assertEqual(result["attributes"]["val"], 1)
+        self.assertEqual(result["value"]["value"]["val"], 1)
 
-        other_attr = result["attributes"]["other"]
-        self.assertEqual(other_attr["attributes"]["val"], 2)
+        other_attr = result["value"]["value"]["other"]
+        self.assertEqual(other_attr["value"]["value"]["val"], 2)
         self.assertTrue(
-            other_attr["attributes"]["other"].startswith("<Circular Reference ")
+            other_attr["value"]["value"]["other"].startswith("<Circular Reference ")
         )
 
     def test_empty_containers(self):
-        self.assertEqual(self.serializer.serialize([]), [])
-        self.assertEqual(self.serializer.serialize(()), [])
-        self.assertEqual(self.serializer.serialize({}), {})
+        self.assertEqual(self.serializer.serialize([])["value"], [])
+        self.assertEqual(self.serializer.serialize(())["value"], [])
+        self.assertEqual(self.serializer.serialize({})["value"], {})
 
         res = self.serializer.serialize(set())
-        self.assertEqual(res["__type"], "set")
-        self.assertEqual(res["items"], [])
+        self.assertEqual(res["__type__"], "set")
+        self.assertEqual(res["value"], [])
 
     def test_exact_max_length(self):
         # exact max length is 5
         l = [1, 2, 3, 4, 5]
-        self.assertEqual(len(self.serializer.serialize(l)), 5)
-        self.assertEqual(self.serializer.serialize(l), [1, 2, 3, 4, 5])
+        res = self.serializer.serialize(l)
+        self.assertEqual(len(res["value"]), 5)
+        self.assertEqual(res["value"], [1, 2, 3, 4, 5])
 
         s = {1, 2, 3, 4, 5}
-        self.assertEqual(len(self.serializer.serialize(s)["items"]), 5)
+        self.assertEqual(len(self.serializer.serialize(s)["value"]), 5)
 
         d = {str(i): i for i in range(5)}
-        self.assertEqual(len(self.serializer.serialize(d)), 5)
-        self.assertNotIn("__truncated", self.serializer.serialize(d))
+        self.assertEqual(len(self.serializer.serialize(d)["value"]), 5)
+        self.assertNotIn("__truncated", self.serializer.serialize(d)["value"])
 
     def test_complex_numbers(self):
         c = 1 + 2j
@@ -176,13 +190,15 @@ class TestSerializer(unittest.TestCase):
     def test_classes_and_instances(self):
         res = self.serializer.serialize(DummyClass)
         self.assertIsInstance(res, dict)
-        self.assertEqual(res["__type"], "type")
-        self.assertTrue(isinstance(res["repr"], str))
+        self.assertEqual(res["__type__"], "type")
+        self.assertIsInstance(res["value"], str)
+        self.assertIn("'__module__': 'test_serializer'", res["value"])
 
     def test_methods_and_builtins(self):
         obj = DummyClass()
         res_method = self.serializer.serialize(obj.__init__)
-        self.assertEqual(res_method, "<method __init__>")
+        self.assertEqual(res_method["__type__"], "method")
+        self.assertEqual(res_method["value"], "__init__")
 
         res_builtin = self.serializer.serialize(len)
         self.assertEqual(res_builtin, "<built-in function len>")
